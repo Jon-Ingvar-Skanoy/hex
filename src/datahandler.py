6b8eddac5b9319d16ce6c2753639cbd36786ee0f
Jon Andreas Bull Larssen <jona.larss@gmail.com>
2024-10-19 01:52:41 +0200

    Organized data- and graph-handling into separate classes

---
+++ src/datahandler.py
@@ -0,0 +1,161 @@
+# datahandler.py
+
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
+
+class DataHandler:
+    def __init__(self,
+                 paths: dict = None,
+                 files: dict = None,
+                 dataset: str = '3x3',
+                 file_ext: str = '.csv',
+                 dataloader: str = 'pd',
+                 n_samples: int = 100000,
+                 balance_data: bool = True,
+                 perform_split: bool = True):
+        
+        self.paths = paths
+        self.files = files
+        self.dataset = dataset
+        self.files_dict = {}
+        self.file_ext = file_ext
+        self.dataloader = dataloader
+        self.n_samples = n_samples
+        self.is_data_balanced = balance_data
+        self.perform_split = perform_split
+
+        self.data = None
+        self.headers = None
+
+        self.X = None
+        self.y = None
+
+        self.load_data_files(self.file_ext)
+        self.load_data()
+        self.prepare_data()
+
+        if balance_data:
+            self.X, self.y = self.balance_data(self.X, self.y, self.n_samples)
+        
+        if perform_split:
+            self.X_train, self.X_test, self.y_train, self.y_test = self.split_data()
+
+
+    def load_data_files(self, file_ext = None):
+        file_ext = file_ext or self.file_ext
+        files = self.paths['data'].glob(f"*{file_ext}")
+        for file in files:
+            short_name = file.stem.split('_')[0]
+            self.files_dict[short_name] = file
+        
+    def load_data(self, dataloader=None, n_samples=None):
+        dataloader = dataloader or self.dataloader
+        n_samples = n_samples or self.n_samples
+        if dataloader == 'pd':
+            self.data = pd.read_csv(self.files_dict[self.files['data']])
+            self.headers = self.data.columns.tolist()
+        elif dataloader == 'np.genfromtxt':
+            with open(self.files_dict[self.files['data']], 'r') as f:
+                self.headers = f.readline().strip().split(',')
+
+            self.data = np.genfromtxt(self.files_dict[self.files['data']],
+                                      delimiter=',',
+                                      skip_header=1,
+                                      dtype=np.int32,
+                                      max_rows=n_samples*2)
+        else:
+            raise ValueError(f"Invalid dataloader: {dataloader}")
+        
+    def prepare_data(self):
+        if self.dataloader == 'pd':
+            X = self.data.iloc[:, :-1]
+            y = self.data.iloc[:, -1]
+            self.X = X.values
+            self.y = y.values
+        elif self.dataloader == 'np.genfromtxt':
+            X = self.data[:, :-1]
+            y = self.data[:, -1]
+            self.X = X
+            self.y = y
+
+    def balance_data(self, X=None, y=None, n_samples=None):
+        X = self.X if X is None else X
+        y = self.y if y is None else y
+        n_samples = n_samples or self.n_samples
+        class_counts = np.bincount(y)
+        majority_class = np.argmax(class_counts)
+        minority_class = np.argmin(class_counts)
+
+        X_majority = X[y == majority_class]
+        X_minority = X[y == minority_class]
+
+        n_samples_per_class = n_samples // 2
+
+        X_majority_downsampled, y_majority_downsampled = resample(
+            X_majority, y[y == majority_class],
+            replace=False,  
+            n_samples=n_samples_per_class, 
+            random_state=42 
+        )
+
+        X_minority_downsampled, y_minority_downsampled = resample(
+            X_minority, y[y == minority_class],
+            replace=False,  
+            n_samples=n_samples_per_class, 
+            random_state=42  
+        )
+
+        X_resampled = np.vstack((X_majority_downsampled, X_minority_downsampled))
+        y_resampled = np.hstack((y_majority_downsampled, y_minority_downsampled))
+
+        return X_resampled, y_resampled
+    
+    def split_data(self, X=None, y=None, test_size=0.2):
+        X = self.X if X is None else X
+        y = self.y if y is None else y
+        return train_test_split(X, y, test_size=test_size, random_state=42)
+    
+    def show_data_info(self):
+        print(f"Data shape: {self.data.shape}")
+        print(f"X shape: {self.X.shape}")
+        print(f"y shape: {self.y.shape}")
+        print(f"Headers: {self.headers}")
+        if self.perform_split:
+            print(f"X_train shape: {self.X_train.shape}")
+            print(f"y_train shape: {self.y_train.shape}")
+            print(f"X_test shape: {self.X_test.shape}")
+            print(f"y_test shape: {self.y_test.shape}")
+
+    def get_class_distribution(self):
+        return np.bincount(self.y)
+
+    def save_graphs(self,
+                    graphs_train,
+                    graphs_test,
+                    X_train = None,
+                    y_train = None,
+                    X_test = None,
+                    y_test = None,
+                    output_file = None):
+
+        X_train = X_train or self.X_train
+        y_train = y_train or self.y_train
+        X_test = X_test or self.X_test
+        y_test = y_test or self.y_test
+        output_file = output_file or f"{self.paths['graphs']}/graphs_{self.dataset}_{self.n_samples}.pkl"
+
+        with open(output_file, 'wb') as f:
+            pickle.dump((graphs_train, graphs_test, X_train, y_train, X_test, y_test), f)
+    
+    def load_graphs(self, input_file = None):
+        if input_file:
+            input_file = f"{self.paths['graphs']}/{input_file}"
+        else:
+            input_file = f"{self.paths['graphs']}/graphs_{self.dataset}_{self.n_samples}.pkl"
+            
+        with open(input_file, 'rb') as f:
+            graphs_train, graphs_test, X_train, y_train, X_test, y_test = pickle.load(f)
+        return graphs_train, graphs_test, X_train, y_train, X_test, y_test
---
+++ src/graphhandler.py
@@ -0,0 +1,197 @@
+# graphhandler.py
+
+import numpy as np
+from GraphTsetlinMachine.graphs import Graphs
+
+class GraphHandler:
+    def __init__(self, 
+                paths: dict = None,
+                files: dict = None,
+                board_size: int = 3,
+                data_array: np.array = None,
+                symbols: list = None,
+                hypervector_size: int = 64,
+                hypervector_bits: int = 2,
+                double_hashing: bool = False,
+                init_with: Graphs = None,
+                verbose_level: int = 0
+                ):
+        
+        self.paths = paths
+        self.files = files
+        self.board_size = board_size
+        self.data_array = data_array
+        self.n_nodes = board_size**2
+        self.symbols = ['RED', 'BLUE','UP', 'DOWN', 'RIGHT','LEFT'] if symbols is None else symbols
+
+        for i in range(board_size):
+            self.symbols.append(f'ROW_{i}')
+            self.symbols.append(f'COL_{i}')
+
+        self.max_index = self.n_nodes - 1
+        self.right_index = self.max_index + 1
+        self.left_index = self.max_index + 2
+        self.down_index = self.max_index + 3
+        self.up_index = self.max_index + 4
+
+        self.hypervector_size = hypervector_size
+        self.hypervector_bits = hypervector_bits
+        self.double_hashing = double_hashing
+        self.init_with = init_with
+        self.verbose_level = verbose_level # 0: no output, 1: minimal output (TODO: tqdm), 2: verbose output, 3: debug output
+
+        if init_with is not None:
+            self.graphs = Graphs(self.data_array.shape[0], init_with=init_with)
+        else:
+            self.graphs = Graphs(self.data_array.shape[0],
+                                symbols=self.symbols,
+                                hypervector_size=self.hypervector_size,
+                                hypervector_bits=self.hypervector_bits,
+                                double_hashing=self.double_hashing)
+ 
+ 
+    def set_n_nodes(self):
+        print(f"Setting number of nodes to {self.n_nodes+4}") if self.verbose_level > 1 else None
+
+        a_range = range(self.data_array.shape[0])
+        for graph_id in a_range:
+            self.graphs.set_number_of_graph_nodes(
+                graph_id=graph_id,
+                number_of_graph_nodes=self.n_nodes+4
+            )
+
+    def get_connections(self, index):
+        print(f"Getting connections for index {index}") if self.verbose_level > 2 else None
+
+        x = index % self.board_size
+        y = index // self.board_size
+        
+        connections = []
+        directions = []
+
+        # Upper connections
+        if y > 0:
+            connections.append(x + (y - 1) * self.board_size)  # Directly above
+            directions.append("dir_up")
+            if y % 2 == 0 and x > 0:  # Even row: diagonal left
+                connections.append(x - 1 + (y - 1) * self.board_size)
+                directions.append("up_left")
+            elif y % 2 == 1 and x < self.board_size - 1:  # Odd row: diagonal right
+                connections.append(x + 1 + (y - 1) * self.board_size)
+                directions.append("up_right")
+            else:
+                connections.append(self.up_index)
+                directions.append("up")
+        else :
+            connections.append(self.up_index)
+            directions.append("up")
+            
+        # Left and right connections
+        if x > 0:
+            connections.append(x - 1 + y * self.board_size)  # Left
+            directions.append("dir_left")
+        else:
+            connections.append(self.left_index)
+            directions.append("left")
+        if x < self.board_size - 1:
+            connections.append(x + 1 + y * self.board_size)  # Right
+            directions.append("dir_right")
+        else:
+            connections.append(self.right_index)
+            directions.append("right")
+
+        # Lower connections
+        if y < self.board_size - 1:
+            connections.append(x + (y + 1) * self.board_size)  # Directly below
+            directions.append("dir_down")
+            if y % 2 == 0 and x > 0:  # Even row: diagonal left
+                connections.append(x - 1 + (y + 1) * self.board_size)
+                directions.append("down_left")
+            elif y % 2 == 1 and x < self.board_size - 1:  # Odd row: diagonal right
+                connections.append(x + 1 + (y + 1) * self.board_size)
+                directions.append("down_right")
+            else:
+                connections.append(self.down_index)
+                directions.append("down")
+        else:
+            connections.append(self.down_index)
+            directions.append("down")
+
+        return connections, directions
+        
+    def add_nodes(self):
+        print("Adding nodes") if self.verbose_level > 1 else None
+
+        self.graphs.prepare_node_configuration()
+        a_range = range(self.data_array.shape[0])
+        b_range = range(self.n_nodes)
+        for graph_id in a_range:
+            for node_id in b_range:
+                nr_neighbours = len(self.get_connections(node_id)[0])
+                self.graphs.add_graph_node(graph_id, node_id, nr_neighbours)
+            self.graphs.add_graph_node(graph_id, self.right_index, self.board_size)
+            self.graphs.add_graph_node(graph_id, self.left_index, self.board_size)
+            self.graphs.add_graph_node(graph_id, self.down_index, self.board_size)
+            self.graphs.add_graph_node(graph_id, self.up_index, self.board_size)
+
+    def add_edges(self):
+        print("Adding edges and properties") if self.verbose_level > 1 else None
+
+        self.graphs.prepare_edge_configuration()
+        a_range = range(self.data_array.shape[0])
+        b_range = range(self.n_nodes+4)
+        for graph_id in a_range:
+            for node_id in b_range:
+                neighbors, directions = self.get_connections(node_id)
+                edge_type = 0
+                if node_id < self.n_nodes:
+                    for neighbor_id, dir in zip(neighbors, directions):
+                        self.graphs.add_graph_node_edge(graph_id, node_id, neighbor_id, dir)
+                    
+                    node_value = self.data_array[graph_id, node_id]
+                    if node_value == 1:
+                        self.graphs.add_graph_node_property(graph_id, node_id, 'RED')
+                    elif node_value == -1:
+                        self.graphs.add_graph_node_property(graph_id, node_id, 'BLUE')
+                    
+                    row = node_id // self.board_size
+                    col = node_id % self.board_size
+
+                    self.graphs.add_graph_node_property(graph_id, node_id, f'ROW_{row}')
+                    self.graphs.add_graph_node_property(graph_id, node_id, f'COL_{col}')
+                
+                if node_id == self.right_index:
+                    neighbors = [i for i in range(self.board_size-1, self.board_size*self.board_size, self.board_size)]
+                    edge_type = 0
+                    for neighbor_id in neighbors:
+                        self.graphs.add_graph_node_edge(graph_id, node_id, neighbor_id, edge_type)
+                    self.graphs.add_graph_node_property(graph_id, node_id, 'RIGHT')
+                if node_id == self.left_index:
+                    neighbors = [i for i in range(0, self.board_size*self.board_size, self.board_size)]
+                    edge_type = 0
+                    for neighbor_id in neighbors:
+                        self.graphs.add_graph_node_edge(graph_id, node_id, neighbor_id, edge_type)
+                    self.graphs.add_graph_node_property(graph_id, node_id, 'LEFT')
+                if node_id == self.down_index:
+                    neighbors = [i for i in range(self.board_size**2-self.board_size, self.board_size**2, 1)]
+                    edge_type = 0
+                    for neighbor_id in neighbors:
+                        self.graphs.add_graph_node_edge(graph_id, node_id, neighbor_id, edge_type)
+                    self.graphs.add_graph_node_property(graph_id, node_id, 'DOWN')
+                if node_id == self.up_index:
+                    neighbors = [i for i in range(self.board_size)]
+                    edge_type = 0
+                    for neighbor_id in neighbors:
+                        self.graphs.add_graph_node_edge(graph_id, node_id, neighbor_id, edge_type)
+                    self.graphs.add_graph_node_property(graph_id, node_id, 'UP')
+        
+    def encode(self):
+        print("Encoding graphs") if self.verbose_level > 1 else None
+        self.graphs.encode()
+        
+    def build_complete_graphs(self):
+        self.set_n_nodes()
+        self.add_nodes()
+        self.add_edges()
+        self.encode()
+        
